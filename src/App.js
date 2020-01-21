dbcbdb3e6f0326c4758567d30b47bb763cc8f6d1
Jorge <jorgebarcos88@gmail.com>
2020-01-21 18:52:21 -0300

    Creando los componentes y routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './components/auth/Login'
+import NuevaCuenta from './components/auth/NuevaCuenta'
+import Proyectos from './components/proyectos/Proyectos'
+
 
 function App() {
   return (
-    <h1>MERNTasks</h1>
+    <Router>
+      <Switch>
+        <Route extact path="/" component={Login} />
+        <Route extact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route extact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
   );
 }
 
