cf7bfa7322755324f0df306b36bbc05fc4e1e567
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-23 20:38:46 -0300

    Trabajando con la Interfaz de Tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -13,8 +13,8 @@ const ListadoTareas = () => {
         <Fragment>
             <h2>Proyecto: Tienda Virtual</h2>
 
-            <ul className="listado-tarea">
-                {tareasProyecto.length ===0 
+            <ul className="listado-tareas">
+                {tareasProyecto.length === 0 
                     ? (<li className="tarea"><p>No hay tareas</p></li>)
                     : tareasProyecto.map(tarea => (
                         <Tarea
@@ -22,7 +22,15 @@ const ListadoTareas = () => {
                         />
                     ))
                 }
+
             </ul>
+            <button
+                type="button"
+                className="btn btn-eliminar"
+                >Eliminar Proyecto &times;
+            </button>
+
+            
 
         </Fragment>
      );
