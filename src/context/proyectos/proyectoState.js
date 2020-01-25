b8a18a794948fc5219f53b765c6cd0d5872376e5
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-25 12:32:48 -0300

    Colocar un Proyecto en Activo

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -8,7 +8,8 @@ import {
     FORMULARIO_PROYECTO, 
     OBTENER_PROYECTOS,
     AGREGAR_PROYECTO,
-    VALIDAR_FORMULARIO
+    VALIDAR_FORMULARIO,
+    PROYECTO_ACTUAL
 } from '../../types';
 
 
@@ -25,7 +26,8 @@ const ProyectoState = props => {
     const initialState = {
         proyectos : [],
         formulario : false,
-        errorformulario: false
+        errorformulario: false,
+        proyecto: null
     }
 
     // Dispatch para ejecutar las acciones
@@ -66,16 +68,28 @@ const ProyectoState = props => {
             type: VALIDAR_FORMULARIO
         })
     }
+
+    // Selecciona el Proyecto que el usuario dio click
+    const proyectoActual = proyectoId => {
+        dispatch({
+            type: PROYECTO_ACTUAL,
+            payload: proyectoId
+        })
+    }
+
+
     return (
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
                 errorformulario: state.errorformulario,
+                proyecto: state.proyecto,
                 mostrarFormulario,
                 obtenerProyectos,
                 agregarProyecto,
-                mostrarError
+                mostrarError,
+                proyectoActual
             }}
         
         >
