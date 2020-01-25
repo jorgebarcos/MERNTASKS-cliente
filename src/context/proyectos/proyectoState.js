0853c3cb863cfb4e33fd34a4a0802b32545be270
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-25 10:58:32 -0300

    Agregando los Proyectos con Context

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -5,7 +5,14 @@ import proyectoReducer from './proyectoReducer';
 import {FORMULARIO_PROYECTO} from '../../types';
 
 const ProyectoState = props => {
+
     const initialState = {
+        proyectos : [
+            { id: 1, nombre: 'Tienda Virtual'},
+            { id: 2, nombre: 'Intranet'},
+            { id: 3, nombre: 'Diseño de Sitio web'},
+            { id: 4, nombre: 'MERN'}
+        ],
         formulario : false
     }
 
@@ -23,6 +30,7 @@ const ProyectoState = props => {
     return (
         <proyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
                 mostrarFormulario
             }}
