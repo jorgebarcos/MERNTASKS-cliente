462d78f0fd14646c02b57aaf3a4d4171812fc774
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-16 20:22:43 -0300

    Insertando Tareas

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -1,7 +1,7 @@
 import React, {useReducer} from 'react';
 import TareaContext from './tareaContext';
 import TareaReducer from './tareaReducer';
-import uuid from 'uuid'
+
 
 import { 
     TAREAS_PROYECTO,
@@ -13,27 +13,11 @@ import {
     ACTUALIZAR_TAREA,
     LIMPIAR_TAREA
 } from '../../types';
-
+import clienteAxios from '../../config/axios';
 
 const TareaState = props => {
     const initialState = {
-        tareas: [
-            {id: 1, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
-            {id: 2, nombre: 'Elegir Colores', estado: false, proyectoId: 2},
-            {id: 3, nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
-            {id: 4, nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
-            {id: 5, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
-            {id: 6, nombre: 'Elegir Colores', estado: false, proyectoId: 2},
-            {id: 7, nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
-            {id: 8, nombre: 'Elegir Plataforma', estado: true, proyectoId: 4},
-            {id: 9, nombre: 'Elegir Colores', estado: false, proyectoId: 1},
-            {id: 10, nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 2},
-            {id: 11, nombre: 'Elegir Plataforma', estado: true, proyectoId: 3},
-            {id: 12, nombre: 'Elegir Colores', estado: false, proyectoId: 4},
-            {id: 13, nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
-            
-        ],
-        tareasproyecto: null,
+        tareasproyecto: [],
         errortarea: false,
         tareaseleccionada: null
     }
@@ -49,15 +33,26 @@ const TareaState = props => {
             type: TAREAS_PROYECTO,
             payload: proyectoId
         })
+        
+        
     }
 
     // Agregar una tarea al proyecto seleccionado
-    const agregarTarea = tarea => {
-        tarea.id = uuid.v4();
-        dispatch({
-            type: AGREGAR_TAREA,
-            payload: tarea
-        })
+    const agregarTarea = async tarea => {
+        
+        try {
+
+            const resultado = await clienteAxios.post('/api/tareas', tarea);
+            dispatch({
+                type: AGREGAR_TAREA,
+                payload: tarea
+            })
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+        
     }
 
     // Valida y muestra un error en caso de que sea necesario
@@ -109,7 +104,6 @@ const TareaState = props => {
     return (
         <TareaContext.Provider
             value={{
-                tareas: state.tareas,
                 tareasproyecto: state.tareasproyecto,
                 errortarea: state.errortarea,
                 tareaseleccionada: state.tareaseleccionada,
